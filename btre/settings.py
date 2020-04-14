0a02762c8dbd3c8cd165158cb5c379cdf3a3d09a
rileylitchfield <rileylitchfield95@gmail.com>
2020-04-13 19:47:05 -0500

    updated allowed hosts

--- btre/settings.py
+++ btre/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
-ALLOWED_HOSTS = ['https://rileyportfolio.herokuapp.com/']
+ALLOWED_HOSTS = ['https://btrealestatedjango.herokuapp.com/']
 
 
 # Application definition
